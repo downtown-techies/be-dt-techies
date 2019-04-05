b6e44e10e626ddf54fed7e394c1fbb77615d7f0c
Blake Burnette <jbburnette2@gmail.com>
2019-04-05 15:10:25 -0400

    added a procfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const db = require('./models/users')
 
 app.use(bodyParser.json())
