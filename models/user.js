ac8f079cd0486bbaafce548611651661687a4806
Blake Burnette <jbburnette2@gmail.com>
2019-04-19 10:02:02 -0400

    Updates model for User

--- models/user.js
+++ models/user.js
@@ -7,6 +7,19 @@ module.exports = function(sequelize, DataTypes) {
     email: DataTypes.STRING,
     active: DataTypes.BOOLEAN,
     type: DataTypes.STRING,
+    address_line_1: DataTypes.STRING,
+    address_line_2: DataTypes.STRING,
+    address_line_3: DataTypes.STRING,
+    address_line_4: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    state_abbr: DataTypes.STRING,
+    postal_code: DataTypes.STRING,
+    country: DataTypes.STRING,
+    preferred_contact: DataTypes.STRING,
+    ph_number: DataTypes.STRING,
+    website: DataTypes.STRING,
+    opt_in: DataTypes.BOOLEAN,
     location: DataTypes.ARRAY(DataTypes.DECIMAL)
   }, {
     classMethods: {
