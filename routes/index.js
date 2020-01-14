64b49335de3cba3d63ce485b12ec994a42fd1784
Blake Burnette <jbburnette2@gmail.com>
2020-01-13 21:17:51 -0500

    fixup

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,12 @@ router.get('/authenticate', authenticate.authenticateUser);
 router.post('/login', authenticate.loginUser);
 
 // ******* users ******** //
-router.get('/users', users.getUsers);
+router.get('/users', middleware.checkAdmin, users.getUsers);
 router.post('/users', users.createUser);
 router.delete('/users/:id', middleware.checkAdmin, users.deleteUser);
 
 // ******* account ******** //
-router.get('/accounts', account.getAccounts);
+router.get('/accounts', middleware.checkAdmin, account.getAccounts);
 router.post('/create_account', account.createAccount);
 router.delete('/delete_account/:id', middleware.checkAdmin, account.deleteAccount);
 
